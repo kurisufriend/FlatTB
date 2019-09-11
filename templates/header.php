3772c73c0af4b9041f6f64e88a150f9f599fac33
sidekek <admin@sidekek.ml>
2019-09-11 08:50:46 -0400

    Add MOTD

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,7 @@
 <?php $root = file_get_contents("data/root") . "/"; ?>
 <div id="header">
     <h1>FlatTB</h1>
+<?php include "data/motd"?>
     <div id="header-banner">
         <a href="<?php echo $root;?>">Home</a> | <a href="<?php echo $root;?>boards.php">Boards</a><br>
     </div>
---
+++ templates/motd.php
@@ -0,0 +1,1 @@
+<div id="motd"><?php include "data/motd";?></div>
