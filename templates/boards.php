20abac72eae13fa3f3eecd012b3dbceea8957ca7
sidekek <admin@sidekek.ml>
2019-09-17 22:44:09 -0400

    fix board links

--- templates/boards.php
+++ templates/boards.php
@@ -2,6 +2,6 @@
 include_once "data/config";
 $boards = array_map("str_getcsv", file("data/boardinfo.csv"));
 foreach ($boards as $lines) {
-    echo "<a href='" . $root . $lines[0] . "'>" . $lines[1] . "</a><br>";
+    echo "<a href='" . $root . "content/" . $lines[0] . "'>" . $lines[1] . "</a><br>";
 }
 ?>
