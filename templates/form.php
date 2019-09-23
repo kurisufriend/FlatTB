ad11ee4ee963ce66e497300dd4a8bfadb600bb88
BuildTools <unconfigured@null.spigotmc.org>
2019-09-23 18:18:16 -0400

    make form look better

--- templates/form.php
+++ templates/form.php
@@ -1,9 +1,11 @@
 <div id="postform" class="infobox">
 <form method="POST" action="submit.php">
-    <strong>Name (opt.)</strong><br><input type="text" size="25" maxlength="25" name="name"><br>
-    <strong>Tripcode (opt.)</strong><br><input type="text" size="25" maxlength="25" name="trip"><br>
-    <strong>Subject</strong><br><input type="text" size="35" maxlength="35" name="subject"><br>
-    <strong>Body</strong><br><textarea rows="10" cols="50" name="body"></textarea><br>
+    <table>
+    <tr><td class="tableEntry"><strong>Name (opt.)</strong></td><td><br><input type="text" size="25" maxlength="25" name="name"><br></td></tr>
+    <tr><td class="tableEntry"><strong>Tripcode (opt.)</strong></td><td><br><input type="text" size="25" maxlength="25" name="trip"><br></td></tr>
+    <tr><td class="tableEntry"><strong>Subject</strong></td><td><br><input type="text" size="35" maxlength="35" name="subject"><br></td></tr>
+    <tr><td class="tableEntry"><strong>Body</strong></td><td><br><textarea rows="10" cols="50" name="body"></textarea><br></td></tr>
+    </table>
     <select name="board">
 <?php
 // lifted straight from templates/boards.php
@@ -13,7 +15,7 @@ foreach ($boards as $lines) {
     echo "<option value='" . $lines[0] . "'>" . $lines[1] . "</option><br>";
 }
 ?>
-    </select>
+    </select><br>
     <input type="submit" name="submit" value="submit">
 </form>
 </div>
