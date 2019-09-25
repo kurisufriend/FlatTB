4da06d8be51d8c467ff171469730baa3c54a205c
BuildTools <unconfigured@null.spigotmc.org>
2019-09-25 19:08:38 -0400

    add time column to catalog

--- templates/catalog.php
+++ templates/catalog.php
@@ -5,10 +5,11 @@ $catalog = array_map("str_getcsv", file("data/globalcatalog.csv"));
 <tr>
 <th>Board</th>
 <th>Post</th>
+<th>Time</th>
 </tr>
 <?php 
 foreach ($catalog as $post) {
-    echo "<tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "</tr>";
+    echo "<tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "<td>" . $post[2] . "</td>" . "</tr>";
 }
 ?>
 </table>
