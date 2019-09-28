40a2088322770b1ad7c42831a63f23b2ed563068
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 18:54:35 -0400

    add "no posts" mode to catalog

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,15 +1,12 @@
 <?php
 $catalog = array_map("str_getcsv", file("data/globalcatalog.csv"));
 ?>
-<table>
-<tr>
-<th>Board</th>
-<th>Post</th>
-<th>Time</th>
-</tr>
-<?php 
-foreach ($catalog as $post) {
-    echo "<tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "<td>" . $post[2] . "</td>" . "</tr>";
+<?php
+if (!empty($catalog)) {
+    foreach ($catalog as $post) {
+        echo "<table><tr><th>Board</th><th>Post</th><th>Time</th></tr><tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "<td>" . $post[2] . "</td>" . "</tr></table>";
+    }
+}else {
+    echo "No Posts Yet";
 }
 ?>
-</table>
