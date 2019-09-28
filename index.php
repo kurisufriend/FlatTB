e52c74722cb87898967ce4f7bae9e0f13ee4794b
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 18:30:12 -0400

    add catalog to index and add flexbox container

--- index.php
+++ index.php
@@ -5,11 +5,16 @@
     </head>
     <body>
 <?php include "templates/header.php"; ?><br>
-        <div id="boards" class="infobox">
+        <div id="main-container">
+            <div id="boards" class="infobox">
 <b>Boards:</b><br><?php include "templates/boards.php"; ?>
-        </div>
-        <div id="latest" class="infobox">
+            </div>
+            <div id="catalog" class="infobox">
+<b>Catalog:</b><br><?php include "templates/catalog.php";?>
+            </div>
+            <div id="latest" class="infobox">
 <b>Latest Post:</b><br><?php $root = file_get_contents("data/latest");echo $root;?>
+            </div>
         </div>
     </body>
 </html>
