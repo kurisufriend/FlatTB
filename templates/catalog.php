90f60d2a89eedecf6cf65b7b714d5fc2c498f891
sidekek <admin@sidekek.ml>
2019-10-01 12:05:15 -0400

    links in catalog

--- templates/catalog.php
+++ templates/catalog.php
@@ -6,7 +6,8 @@ include_once "data/config.php";
 if (!empty($catalog)) {
     echo "<table><tr><th>Board</th><th>Post</th><th>Time</th></tr>";
     foreach ($catalog as $post) {
-        echo "<tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "<td>" . $post[2] . "</td>" . "</tr>";
+        $link = $root . "view.php?board=" . $post[0] . "&" . "thread=" . $post[1];
+        echo "<tr><td>" . $post[0] . "</td>" . "<td><a href='" . $link . "'>" . $post[1] . "</a></td>" . "<td>" . $post[2] . "</td>" . "</tr>";
     }
     echo "</table>";
 }else {
