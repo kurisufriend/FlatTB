ac63e15ad67b061bdf2ab64f8f1e21f0b9be2860
sidekek <admin@sidekek.ml>
2019-10-01 12:00:41 -0400

    catalog fix

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,11 +1,14 @@
 <?php
 $catalog = array_map("str_getcsv", file("data/globalcatalog.csv"));
+include_once "data/config.php";
 ?>
 <?php
 if (!empty($catalog)) {
+    echo "<table><tr><th>Board</th><th>Post</th><th>Time</th></tr>";
     foreach ($catalog as $post) {
-        echo "<table><tr><th>Board</th><th>Post</th><th>Time</th></tr><tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "<td>" . $post[2] . "</td>" . "</tr></table>";
+        echo "<tr><td>" . $post[0] . "</td>" . "<td>" . $post[1] . "</td>" . "<td>" . $post[2] . "</td>" . "</tr>";
     }
+    echo "</table>";
 }else {
     echo "No Posts Yet";
 }
