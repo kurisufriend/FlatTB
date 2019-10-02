d85f68a4968a88d211c8eb60f0b481060818dd82
sidekek <admin@sidekek.ml>
2019-10-02 19:53:09 -0400

    url encode catalog

--- templates/catalog.php
+++ templates/catalog.php
@@ -6,6 +6,7 @@ include_once "data/config.php";
 if (!empty($catalog)) {
     echo "<table><tr><th>Board</th><th>Post</th><th>Time</th></tr>";
     foreach ($catalog as $post) {
+        $post[1] = rawurldecode($post[1]);
         $link = $root . "view.php?board=" . $post[0] . "&" . "thread=" . $post[1];
         echo "<tr><td>" . $post[0] . "</td>" . "<td><a href='" . $link . "'>" . $post[1] . "</a></td>" . "<td>" . $post[2] . "</td>" . "</tr>";
     }
