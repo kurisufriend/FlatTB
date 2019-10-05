c325fa8fcd180af31ada004e45dceda4f365583e
sidekek <admin@sidekek.ml>
2019-10-05 15:25:21 -0400

    only display 10 latest posts in main catalog

--- templates/catalog.php
+++ templates/catalog.php
@@ -5,10 +5,15 @@ include_once "data/config.php";
 <?php
 if (!empty($catalog)) {
     echo "<table><tr><th>Board</th><th>Post</th><th>Time</th></tr>";
+    $i = 0;
     foreach ($catalog as $post) {
+        if ($i >= 10) {
+            break;
+        }
         $post[1] = rawurldecode($post[1]);
         $link = $root . "view.php?board=" . $post[0] . "&" . "thread=" . $post[1];
         echo "<tr><td>" . $post[0] . "</td>" . "<td><a href='" . $link . "'>" . $post[1] . "</a></td>" . "<td>" . $post[2] . "</td>" . "</tr>";
+        $i++;
     }
     echo "</table>";
 }else {
