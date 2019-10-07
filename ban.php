2894e0a96e04f2e5401e23860c903f3efe33614d
sidekek <admin@sidekek.ml>
2019-10-06 21:23:37 -0400

    b& baby

--- ban.php
+++ ban.php
@@ -1,5 +1,8 @@
 <?php
+include_once "lib/db.php";
 $client = $_SERVER['REMOTE_ADDR'];
 echo "IP Address: [" . $client. "]";
-
+if (csv_search("data/baninfo.csv", $client)) {
+    echo "banned";
+}
 ?>
--- lib/db.php
+++ lib/db.php
@@ -3,4 +3,18 @@ function csv_add_top($csv, $line) {
     $ex = file_get_contents($csv);
     file_put_contents($csv, $line . $ex);
 }
+
+function csv_search($csv, $needle) {
+    $haystack = array_map("str_getcsv", file($csv));
+    $b = 0;
+    foreach ($haystack as $line) {
+        if ($line[0] == $needle) {
+            $b = 1;
+            return true;
+        }
+    }
+    if (!$b = 1) {
+        return false;
+    }
+}
 ?>
