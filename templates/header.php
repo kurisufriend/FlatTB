04c4a5ba2617325d6f3635abbb5f0a4321dcdda2
sidekek <admin@sidekek.ml>
2019-10-19 16:00:57 -0400

    improve css

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <?php include_once "data/config.php"; ?>
 <div id="header">
     <h1><?php echo $title;?></h1>
-<?php include "data/motd"?>
+<span id="header-motd"><?php include "data/motd"?></span>
     <div id="header-banner">
         <a href="<?php echo $root;?>">Home</a> | <a href="<?php echo $root;?>boards.php">Boards</a> | <a href="<?php echo $root;?>post.php">New Post</a><br>
     </div>
