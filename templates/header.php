f261856d430b2bc9107ba783f1a279e342cebfbe
sidekek <admin@sidekek.ml>
2019-11-07 11:08:51 -0500

    linky header

--- templates/header.php
+++ templates/header.php
@@ -1,6 +1,6 @@
 <?php include_once "data/config.php"; ?>
 <div id="header">
-    <h1><?php echo $title;?></h1>
+    <h1><?php echo "<a href='" . $root . "'>" . $title . "</a>";?></h1>
 <span id="header-motd"><?php include "data/motd"?></span>
     <div id="header-banner">
         <a href="<?php echo $root;?>">Home</a> | <a href="<?php echo $root;?>boards.php">Boards</a> | <a href="<?php echo $root;?>post.php">New Post</a><br>
